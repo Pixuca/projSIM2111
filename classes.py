548c6a1b7a626326c054be9f5cbd14e432644923
Unknown <marcelo.macedo330@hotmail.com>
2018-04-18 16:49:21 -0300

    Movimento do lado esquerdo do campo adicionado
    O movimento do lado esquerdo foi implementado ao código e os erros de movimentação foram corrigidos.

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 import random
 from trajetoria import *
 class robot:
-    x = random.randint(389, 750)
+    x = random.randint(20, 389)
     x2 = trajX[0]
     y = random.randint(30, 450)
     espessura = 20
@@ -24,3 +24,4 @@ class bola:
     x = 390
     y = 245
     espessura = 5
+    color = (255, 102, 0)
--- pypy.py
+++ pypy.py
@@ -19,16 +19,15 @@ done = False
 	30, 750 para o robô ter possibilidade de aparecer nos dois lados do campo
 
 ------------------------------------------------'''
-
 x, y = 750/9, 450/6
 distT = (x + y)/2
 print("A cada %.2f pixels se tem 1 (um) metro. " % distT)
 distancia = (robot.x - bola.x) + (robot.y - bola.y)
-print("Distância (em pixels): %d" % distancia)
+print("Distância (em pixels): %dp" % distancia)
 if (robot.x < bola.x):
 	if (robot.y > bola.y):
 		distancia = (bola.x - robot.x) + (robot.y - bola.y)
-		print("Distância (em pixels): %d" % distancia)
+		print("Distância (em pixels): %dp" % distancia)
 		distanciaT = distancia/distT
 		print("Distância (em metros): %.2fm" % distanciaT)
 def sweepRight():
@@ -36,47 +35,95 @@ def sweepRight():
 		if (robot.y > bola.y):
 			distancia = (robot.x - bola.x) + (robot.y - bola.y)
 			distanciaT = distancia/distT
-			print("Distância (em metros): %.2f" % distanciaT)
+			print("Distância: %.2fm" % distanciaT)
 		elif (robot.y < bola.y):
 			distancia = (robot.x - bola.x) + (bola.y - robot.y)
 			distanciaTotal = distancia/distT
-			print("Distância (em metros): %.2f" % distanciaTotal)
+			print("Distância: %.2fm" % distanciaTotal)
 	if(robot.x > bola.x + 5):
 		robot.x -= 1
 		colision.x -= 1
 		kick.x -= 1
-		time.sleep(0.007)
 
 	if(robot.y > bola.y - 10):
 		robot.y -= 1
 		colision.y -= 1
 		kick.y -= 1
-		time.sleep(0.007)
+
 	elif (robot.y < bola.y - 10):
 		robot.y += 1
 		colision.y += 1
 		kick.y += 1
-		time.sleep(0.007)
-	if (colision.x - bola.x <= 15 and colision.y - bola.y <= 15):
-		forca = random.randint(40, 100)
-		chance = random.randint(0, 2)
-		if (chance == 0):
-				bola.x -= forca
+	if (kick.x - bola.x <= 15):
+		if (bola.y > robot.y):
+			robot.y += 1
+			colision.y += 1
+			kick.y += 1
+		if (kick.y - bola.y <= 15 and kick.y - bola.y > 0):
+			forca = random.randint(40, 100)
+			chance = random.randint(0, 2)
+			if (chance == 0):
 				bola.y += forca
-		elif (chance == 1):
+				bola.x -= forca
+
+			elif (chance == 1):
 				bola.x -= forca
 				bola.y -= forca
-		elif (chance == 2):
+
+			elif (chance == 2):
 				bola.x -= forca
 
+	if (robot.x <= 0):
+		robot.x = 0
+		colision.x = 0
+		kick.x = 8
 
 def sweepLeft():
-	if ((colision.x + bola.x) <= colision.diametro):
-		print("Estou vendo a bola! Minha distância até ela é de X = %d" % ((colision.x + bola.x)))
-	if (robot.x < bola.x+5):
+	if (bola.x > robot.x):
+		if (robot.y > bola.y):
+			distancia = (bola.x - robot.x) + (robot.y - bola.y)
+			distanciaT = distancia/distT
+			print("Distância: %.2fm" % distanciaT)
+		elif (robot.y < bola.y):
+			distancia = (bola.x - robot.x) + (bola.y - robot.y)
+			distanciaTotal = distancia/distT
+			print("Distância: %.2fm" % distanciaTotal)
+
+
+	#parte da movimentação do lado esqurdo do campo
+	if (robot.x < bola.x - 20):
 		robot.x += 1
 		colision.x += 1
 		kick.x += 1
+
+	if (robot.y < bola.y - 10):
+		robot.y += 1
+		colision.y += 1
+		kick.y += 1
+	elif (robot.y > bola.y - 10):
+		robot.y -= 1
+		colision.y -= 1
+		kick.y -= 1
+	if (bola.x - kick.x <= 15):
+		if(bola.y > robot.y):
+			robot.y += 1
+			colision.y += 1
+			kick.y += 1
+		if (kick.y - bola.y <= 15 and kick.y - bola.y > 0):
+			forca = random.randint(40, 100)
+			chance = random.randint (0, 2)
+			if (chance == 0):
+				bola.y += forca
+				bola.x += forca
+			elif (chance == 1):
+				bola.x += forca
+				bola.y -= forca
+			elif (chance == 2):
+				bola.x += forca
+	if (robot.x >= 750):
+		robot.x = 750
+		colision.x = 750
+		kick.x = 758
 
 while not done:
 	for event in pygame.event.get():
@@ -87,7 +134,6 @@ while not done:
 		elif robot.x < bola.x:
 			sweepLeft()
 	screen.fill((0, 0, 0))
-	color = (255, 102, 0)
 	robColor = (0, 0, 255)
 	golColor = (255, 255, 255)
 	areaColor = (0, 0, 0)
@@ -95,7 +141,7 @@ while not done:
 
 	colisionRange = pygame.draw.circle(screen, colision.colisionColor, (colision.x, colision.y), colision.diametro, colision.espessura)
 	kickRange = pygame.draw.circle(screen, kick.kickColor, (kick.x, kick.y), kick.diametro, kick.espessura)
-	ball = pygame.draw.circle(screen, color, (bola.x, bola.y), bola.espessura)
+	ball = pygame.draw.circle(screen, bola.color, (bola.x, bola.y), bola.espessura)
 	robot = pygame.draw.rect(screen, robColor, pygame.Rect(robot.x, robot.y, 20, 20))
 	goal2 = pygame.draw.rect(screen, golColor, pygame.Rect(745, 223.5, 1, 50)) #gol da direita
 	goal1 = pygame.draw.rect(screen, golColor, pygame.Rect(33, 223.5, 1, 50)) #gol da esquerda
